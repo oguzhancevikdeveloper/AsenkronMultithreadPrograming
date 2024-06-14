d2d1a2d0870b0f3f93c24fa088b49ec2bed5df4a
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-06-14 13:16:35 +0300

    Add project files.

---
+++ Chapter_01/Program.cs
@@ -0,0 +1,23 @@
+﻿X();
+Y();
+
+
+Console.ReadLine();
+
+async void X()
+{
+    await Task.Run(() =>
+    {
+        for (int i = 0; i < 20; i++)
+            Console.WriteLine($"X --> {i}");
+    });
+}
+
+async void Y()
+{
+    await Task.Run(() =>
+    {
+        for (int i = 0; i < 20; i++)
+            Console.WriteLine($"Y --> {i}");
+    });
+}
---
+++ Chapter_18/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+
+#region Senkron
+//IEnumerable<int> GetNumbers()
+//{
+//    for (int i = 0; i < 30; i++)
+//    {
+//        Thread.Sleep(1000);
+//        yield return i;
+//    }
+//}
+//foreach (var item in GetNumbers())
+//    Console.WriteLine(item);
+
+#endregion
+
+#region Asenkron
+
+//async IAsyncEnumerable<int> GetNumbersAsync()
+//{
+//    for (int j = 0; j < 30; j++)
+//    {
+//        await Task.Delay(1000);
+//        yield return j;
+//    }
+//}
+
+//await foreach (var item in GetNumbersAsync())
+//    Console.WriteLine(item);
+#endregion
+
+#region Custom
+
+// await foreach ifadesi, üzerinde çalışacağı nesnenin IAsyncEnumerable<T> arayüzünü uyguladığını varsayar. Bu arayüz, GetAsyncEnumerator metodunu içerir ve bu metod bir IAsyncEnumerator<T> döner.
+NumberList numberlist = new NumberList();
+
+
+
+await using (var enumarator = numberlist.GetAsyncEnumerator())
+{
+    while (await enumarator.MoveNextAsync())
+    {
+        var number = enumarator.Current;
+        Console.WriteLine(number);
+    }
+}
+
+
+var t = numberlist.GetAsyncEnum();
+
+await foreach (var number in numberlist)
+    Console.WriteLine(number);
+
+class NumberList : IAsyncEnumerable<int>
+{
+    List<int> numbers = new() { 1, 3, 5, 7, 9 };
+
+    public async IAsyncEnumerator<int> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        foreach (var number in numbers)
+        {
+            await Task.Delay(500);
+            yield return number;
+        }
+
+    }
+
+    public async IAsyncEnumerator<int> GetAsyncEnum()
+    {
+        foreach (var number in numbers)
+        {
+            await Task.Delay(500);
+            yield return number;
+        }
+
+    }
+
+}
+
+#endregion
